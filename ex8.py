b098323284b00aebfc52054fddfd5b601efcc137
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-09 18:41:44 -0800

    excerise 8 completed

---
+++ ex8.py
@@ -0,0 +1,7 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format("Do the impossible", "See the the invisible", "Row Row", "Fight The Power"))
